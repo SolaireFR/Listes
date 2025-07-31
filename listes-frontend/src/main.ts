88b33e4c7d71e137059666321426a221c63138b5
SolaireFR <100956531+SolaireFR@users.noreply.github.com>
2025-07-31 09:23:50 +0200

    Update main.ts

--- listes-frontend/src/main.ts
+++ listes-frontend/src/main.ts
@@ -2,8 +2,12 @@ import 'reflect-metadata';
 import 'zone.js'; // Included with Angular CLI.
 
 import { bootstrapApplication } from '@angular/platform-browser';
+import { environment } from '../../environments/environment';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+
+
+console.log(`APIURL:${environment.apiUrl}`);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
