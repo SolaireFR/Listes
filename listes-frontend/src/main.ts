e5ee22a1971f67f147e74c0ac59ae213253a90e6
SolaireFR <100956531+SolaireFR@users.noreply.github.com>
2025-07-31 09:35:11 +0200

    Update main.ts

--- listes-frontend/src/main.ts
+++ listes-frontend/src/main.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import 'zone.js'; // Included with Angular CLI.
 
 import { bootstrapApplication } from '@angular/platform-browser';
-import { environment } from '../../environments/environment';
+import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
